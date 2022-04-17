d6580fc6127da813b26c3cf473476867a559c254
Вячеслав Бородин <vikc003@mail.ru>
2022-04-17 11:37:04 +0500

    install drf

--- carsite/cars/admin.py
+++ carsite/cars/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cars, Category
 
-# Register your models here.
+admin.site.register(Cars)
+admin.site.register(Category)
--- carsite/cars/views.py
+++ carsite/cars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Cars, Category
+from .serializers import CarsSerializer
 
-# Create your views here.
+class CarsAPIView(generics.ListAPIView):
+    queryset = Cars.objects.all()
+    serializer_class = CarsSerializer
