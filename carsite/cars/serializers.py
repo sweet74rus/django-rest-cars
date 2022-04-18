ce0600019c63ee49912f883e8c680fcbddd3b008
Вячеслав Бородин <vikc003@mail.ru>
2022-04-18 11:12:47 +0500

    serialize test

--- carsite/cars/serializers.py
+++ carsite/cars/serializers.py
@@ -1,8 +1,30 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Cars
 
-class CarsSerializer(serializers.ModelSerializer):
+class CarsModel:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
 
-    class Meta:
-        model = Cars
-        fields = ('title', 'cat_id')
+class CarsSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+
+def encode():
+    model = CarsModel('Mercedes-Benz C180', 'description: C180')
+    model_sr = CarsSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+def decode():
+    stream = io.BytesIO(b'{"title": "Mercedes-Benz C180", "description": "description: C180"}')
+    data = JSONParser().parse(stream)
+    serializer = CarsSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
--- carsite/cars/views.py
+++ carsite/cars/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Cars, Category
 from .serializers import CarsSerializer
 
-class CarsAPIView(generics.ListAPIView):
-    queryset = Cars.objects.all()
-    serializer_class = CarsSerializer
+class CarsAPIView(APIView):
+    def get(self, request):
+        l = Cars.objects.all().values()
+        return Response({'cars': list(l)})
+
+# class CarsAPIView(generics.ListAPIView):
+#     queryset = Cars.objects.all()
+#     serializer_class = CarsSerializer
